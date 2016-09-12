a3f1400c37fddb630fbd733da59ce0e00f4149c4
turtleDev <saravjeetamansingh@gmail.com>
2016-09-12 12:07:25 +0530

    add working draft v2

--- lasso.py
+++ lasso.py
@@ -18,6 +18,7 @@ import copy
 import json
 import random
 import re
+import urllib
 
 import requests
 import lxml.etree
@@ -32,127 +33,141 @@ class config:
     API_URI = 'https://en.wikipedia.org/w/api.php?action=parse&format=json'
 
 
-def scaffold_payload(payload):
+def fetch(topic, chunks=5):
+    '''fetches data about a topic'''
 
-    defaults = {
+    count = 0
+    topic = urllib.unquote(topic)
+    payload = {
+        'page': topic,
         'prop': 'text|images',
         'uselang': 'en'}
 
-    payload = copy.deepcopy(payload)
-    payload.update(defaults)
-    return payload
+    while count < chunks:
 
+        print "fetching {}".format(payload['page'])
+        response = requests.post(config.API_URI, payload)
 
-def fetch(topic):
-    '''fetches data about a topic'''
-    payload = {'page': topic}
-    payload = scaffold_payload(payload)
+        # TODO: add checks against response code
+        data = json.loads(response.content)
+        data = data['parse']['text']['*']
 
-    response = requests.post(config.API_URI, payload)
+        selector = lxml.etree.HTML(data)
 
-    # TODO: add checks against response code
-    data = json.loads(response.content)
-    data = data['parse']['text']['*']
-
-    selector = lxml.etree.HTML(data)
-
-    # I'm really not sure how or why this redirect happens, but I'm guessing
-    # it redirects you to another page that may have been merged with another,
-    # so we basically try to fetch the data from that other page.
-    try:
-        redirect = selector.xpath(
-            '//*[contains(@class,"redirectMsg")]/a/text()').pop()
-
-        # recursively search for the next suggestion
-        # XXX: can this his the recursion limit? find a better alternative
-        return fetch(redirect)
-    except IndexError:
-        pass
-
-    # handle disambiguations
-    disambig = selector.xpath('//*[@id="disambigbox"]')
-    if disambig:
-        disambig_links = selector.xpath(
-            '//a[@title and '
-            'not(contains(@href,":")) and '
-            'not(contains(@href,"wikitionary")) and '
-            'not(contains(@href,"wikimedia")) and '
-            'not(@class="external") and '
-            'not(@class="new")]')
-
-        next_link = random.choice(disambig_links)
-
-        # this _may_ throw an IndexError
-        title = re.search(
-            "/wiki\/(.*)$", next_link).groups()[0].replace("\#.*", "")
-
-        # how I wish python implmented tail call elimination :(
-        return fetch(title)
-
-    first = selector.xpath('//p')
-
-    # filter out those paragraphs that have a link with a title attribute
-    first = filter(lambda e: e.xpath('a[@title]'), first)
-
-    # choose a random para from the first five paragraphs
-    first = random.choice(first[:5])
-
-    links = None
-    if len(first):
-        links = selector.xpath(
-            '//a[@title and '
-            'not(contains(@href,":")) and '
-            'not(contains(@href,"wiktionary")) and '
-            'not(contains(@href,"wikimedia")) and '
-            'not(@class="external") and '
-            'not(@class="new") and '
-            'not(contains(@title,"Edit"))]')
-    else:
-        # this block makes *NO* sense to me
-
-        # convert the para to html
-        first = lxml.html.tostring(first)
-
+        # I'm really not sure how or why this redirect happens, but I'm guessing
+        # it redirects you to another page that may have been merged with another,
+        # so we basically try to fetch the data from that other page.
         try:
-            blob = re.search(".*?title=.*?\.(\s|\n|$)", first).groups()[0]
-        except AttributeError:
-            blob = ''
+            redirect = selector.xpath(
+                '//*[contains(@class,"redirectMsg")]/a/text()').pop()
 
-        links = lxml.etree.HTML('<p>' + blob + '</p>').xpath(
-            '//a[@title and '
-            'not(contains(@href,":") and '
-            'not(contains(@href,"wiktionary") and '
-            'not(contains(@href,"wikimedia") and '
-            'not(@class="external") and '
-            'not(@class="new") and '
-            'not(contains(@title,"Edit")]')
+            # # recursively search for the next suggestion
+            # # XXX: can this his the recursion limit? find a better alternative
+            # return fetch(redirect)
+            payload['page'] = redirect
+            continue
+        except IndexError:
+            pass
 
-    # if we can't find a valid link, follow an old link
-    if not links:
-        links = selector.xpath(
-            '//p/a[@title and '
-            'not(contains(@href,":") and '
-            'not(contains(@href,"wiktionary") and '
-            'not(contains(@href,"wikimedia") and '
-            'not(@class="external") and '
-            'not(@class="new") and'
-            'not(contains(@title,"Edit")]')
+        # handle disambiguations
+        disambig = selector.xpath('//*[@id="disambigbox"]')
+        if disambig:
+            disambig_links = selector.xpath(
+                '//a[@title and '
+                'not(contains(@href,":")) and '
+                'not(contains(@href,"wikitionary")) and '
+                'not(contains(@href,"wikimedia")) and '
+                'not(@class="external") and '
+                'not(@class="new")]')
 
-    link = random.choice(links)
-    link = link.xpath('@href').pop()
-    title = re.search("/wiki\/(.*)$", link).groups()[0]
-    title = re.sub("\#.*", "", title)
-    images = selector.xpath('//*[contains(@class,"thumbinner")]')
+            next_link = random.choice(disambig_links)
 
-    # TODO: images
+            # this _may_ throw an IndexError
+            title = re.search(
+                "/wiki\/(.*)$", next_link).groups()[0].replace("\#.*", "")
 
-    result = lxml.html.tostring(first)
+            # # how I wish python implmented tail call elimination :(
+            # return fetch(title)
+            payload['page'] = title
+            continue
 
-    # get rid of refs
-    result = re.sub("<sup.*?>.*?</sup>", "", result)
+        first = selector.xpath('//p//a[@title]/ancestor::p')
 
-    # extract the text
-    result = re.sub('<\/?[^<>]*>', '', result)
+        # # filter out those paragraphs that have a link with a title attribute
+        # first = filter(lambda e: e.xpath('a[@title]'), first)
 
-    result = to_unicode(result)
-    return result
+        # choose a random para from the first five paragraphs
+        try:
+            first = random.choice(first[:5])
+        except IndexError:
+            first = []
+
+        links = None
+
+        if len(first) == 0:
+            links = selector.xpath(
+                '//a[@title and '
+                'not(contains(@href,":")) and '
+                'not(contains(@href,"wiktionary")) and '
+                'not(contains(@href,"wikimedia")) and '
+                'not(@class="external") and '
+                'not(@class="new") and '
+                'not(contains(@title,"Edit"))]')
+        else:
+            # this block makes *NO* sense to me
+
+            # convert the para to html
+            first = lxml.html.tostring(first)
+
+            try:
+                blob = re.search(".*?title=.*?\.(\s|\n|$)", first).groups()[0]
+            except AttributeError:
+                blob = ''
+
+            links = lxml.etree.HTML('<p>' + blob + '</p>').xpath(
+                '//a[@title and '
+                'not(contains(@href,":")) and '
+                'not(contains(@href,"wiktionary")) and '
+                'not(contains(@href,"wikimedia")) and '
+                'not(@class="external") and '
+                'not(@class="new") and '
+                'not(contains(@title,"Edit"))]')
+
+        # if we can't find a valid link, follow an old link
+        if not links:
+            links = selector.xpath(
+                '//p//a[@title and '
+                'not(contains(@href,":")) and '
+                'not(contains(@href,"wiktionary")) and '
+                'not(contains(@href,"wikimedia")) and '
+                'not(@class="external") and '
+                'not(@class="new") and'
+                'not(contains(@title,"Edit"))]')
+
+        link = random.choice(links)
+        link = link.xpath('@href').pop()
+        title = re.search("/wiki\/(.*)$", link).groups()[0]
+        title = re.sub("\#.*", "", title)
+        images = selector.xpath('//*[contains(@class,"thumbinner")]')
+
+        # TODO: images
+
+        if isinstance(first, lxml.etree._Element):
+            result = lxml.html.tostring(first)
+        else:
+            result = first
+
+        if isinstance(result, list) is False:
+
+            # get rid of refs
+            result = re.sub("<sup.*?>.*?</sup>", "", result)
+
+            # extract the text
+            result = re.sub('<\/?[^<>]*>', '', result)
+
+            result = to_unicode(result)
+            yield result
+            count += 1
+
+        title = urllib.unquote(title)
+        payload['page'] = title
